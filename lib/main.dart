2424f626a704aa81b50bfb3ef3d3b661d2fb883e
Imran Ali Khan <70568289+MKServices1@users.noreply.github.com>
2022-05-31 10:25:30 +0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_login_sign_up/views/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
